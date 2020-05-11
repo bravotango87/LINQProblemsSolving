9f75cdb3704cc3bd91efd379856051c8780631be
Brad Toney <bravotango87@gmail.com>
2020-05-11 13:10:32 -0400

    added list to LINQ class.

--- LINQ.cs
+++ LINQ.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LINQProblems
 {
     class LINQ
     {
+        // member variables (Has a)
+
+
         public List<string> words = new List<string>() {"the", "bike", "this", "it", "tenth", "mathematics"};
         public List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+        public List<string> classGrades = new List<string>()
+        {
+            "80, 100, 92, 89, 65",
+            "93, 81, 78, 84, 69",
+            "73, 88, 83, 99, 64",
+            "98, 100, 66, 74, 55"
+        };
+
+      // constructor (Spawner)
+
+
+     // member methods (Can do)
 
     }
 }
